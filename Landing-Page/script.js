3bcab8c0606dee38eea7381590eb6a5992edb824
Akshat Verma <112319520+Akshat162001@users.noreply.github.com>
2023-07-31 09:13:07 +0530

    Update script.js

--- Landing-Page/script.js
+++ Landing-Page/script.js
@@ -1,7 +1,9 @@
 const check = document.querySelector("#check");
+
 const navLinks = document.querySelector("#nav-links");
 
 check.addEventListener("click", () => {
+    
     if (check.checked) {
         navLinks.style.left = '0';
     } else {
